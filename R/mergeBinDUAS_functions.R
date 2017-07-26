cdcf531637111f59b7aa98ebeaca917c10a9fb8b
Javier Iserte <jiserte@leloir.org.ar>
2017-07-26 17:05:07 -0300

    Fix a bug in mergeBinDUAS
    Fixed a bug where mergeBinDUAS was slow, and junction names where
    integers.
    
    
    Signed-off-by: Javier Iserte <jiserte@leloir.org.ar>

--- R/mergeBinDUAS_functions.R
+++ R/mergeBinDUAS_functions.R
@@ -5,13 +5,24 @@
   bdu <- binsDU( du )
   bas$event <- NULL
   
-  if ( is.null( contrast )) contrast <- .getDefaultContrasts( targets$condition )
+  bas2 <- as.data.frame( matrix( NA, nrow = nrow( bdu ), ncol = ncol( bas ) ) )
+  rownames( bas2 ) <- rownames( bdu )
+  colnames( bas2 ) <- colnames( bas )
   
-  psir <- t( bas[ rownames(bdu) , getConditions( targets ) ] )
+  bas2[ intersect( rownames( bas ), rownames( bdu ) ), ] <- bas[ intersect( rownames( bas ), rownames( bdu ) ),  ]
+  bas2[,'J1'] <- levels( bas[,'J1'] )[ bas2[,'J1'] ]
+  bas2[,'J2'] <- levels( bas[,'J2'] )[ bas2[,'J2'] ]
+  bas2[,'J3'] <- levels( bas[,'J3'] )[ bas2[,'J3'] ]
+  
+  if ( is.null( contrast )) contrast <- .getDefaultContrasts( targets$condition )
+
+  psir <- t( bas2[ , getConditions( targets ) ] )
   psir <- psir * contrast
   psir <- t( psir[ contrast != 0, ] )
   psir <- data.frame( delta = rowSums( psir ) )
   
-  cbind( bdu, bas[rownames(bdu),], psir )
+  r <- cbind( bdu, bas2[ ,], psir )
+  colnames( r ) <- c( colnames( bdu), colnames( bas2), 'delta' )
+  return( r )
   
 }
